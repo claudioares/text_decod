ec29655eab11a3577b41aa75615444a85f53f7dc
claudioares <claudiojasoares.dev@gmail.com>
2024-02-01 15:05:52 -0300

    refatorando para programação orientada a objetos (OOP)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import TextProcess from './scriptClass';
+const textprocess = new TextProcess();
+
+const buttonCripty = document.querySelector('.cripty');
+
+buttonCripty.addEventListener('click', ()=>{
+  alert("buttom cripty")
+  // textprocess.handleCripty();
+})
--- src/script.js
+++ src/script.js
@@ -1,41 +1,54 @@
-const text = document.querySelector(".text_area textarea");
-const cripty = document.querySelector(".cripty");
-const descripty = document.querySelector(".descripty");
-const divWordCripty = document.querySelector(".content_text_detected");
-const contentNotFound = document.querySelector(".content_not_found");
-const wordCripty = document.querySelector(".content_text_detected p");
-const copy = document.querySelector(".copy");
-const alertCopy = document.querySelector(".alert_copy");
-const clean = document.querySelector(".clean");
-const modal = document.querySelector(".div_modal");
-const modalSpan = document.querySelector(".content_modal span");
-const button_y = document.querySelector(".button_y");
-const button_n = document.querySelector(".button_n");
+export default class TextProcess {
+  constructor() {
+    this.text = document.querySelector(".text_area textarea");
+    this.criptyButton = document.querySelector(".cripty");
+    this.descriptyButton = document.querySelector(".descripty");
+    this.divWordCripty = document.querySelector(".content_text_detected");
+    this.contentNotFound = document.querySelector(".content_not_found");
+    this.wordCripty = document.querySelector(".content_text_detected p");
+    this.copyButton = document.querySelector(".copy");
+    this.alertCopy = document.querySelector(".alert_copy");
+    this.cleanButton = document.querySelector(".clean");
+    this.modal = document.querySelector(".div_modal");
+    this.modalSpan = document.querySelector(".content_modal span");
+    this.button_y = document.querySelector(".button_y");
+    this.button_n = document.querySelector(".button_n");
 
+    this.inicialize();
+  };
 
-function detectedCharacters (array) {
+  inicialize() {
+    this.criptyButton.addEventListener('click', this.handleCripty.bind(this));
+    this.copyButton.addEventListener('click', this.handleCopy.bind(this));
+    this.cleanButton.addEventListener('click', this.handleClean.bind(this));
+    this.descriptyButton.addEventListener('click', this.handleDescripty.bind(this));
+    this.button_y.addEventListener('click', this.handleYes.bind(this));
+    this.button_n.addEventListener('click', this.handleNo.bind(this));
+  };
+
+  detectedCharacters(array) {
     const forbiddenCharacters = [
-        'A', 'Á', 'À', 'Â', 'Ã', 'Ä',
-        'B',
-        'C', 'Ç',
-        'D',
-        'E' ,'É', 'È', 'Ê', 'Ë',
-        'F', 'G', 'H',
-        'I', 'Í', 'Ì', 'Î', 'Ï',
-        'J', 'L', 'M', 'N',
-        'O','Ó', 'Ò', 'Ô', 'Õ', 'Ö',
-        'P', 'Q', 'R', 'S', 'T',
-        'U', 'Ú', 'Ù', 'Û', 'Ü',
-        'V', 'X', 'Z',
-        'Y', 'W', 'K',
-        'á', 'à', 'â', 'ã', 'ä',
-        'ç',
-        'é', 'è', 'ê', 'ë',
-        'í', 'ì', 'î', 'ï',
-        'ó', 'ò', 'ô', 'õ', 'ö',
-        'ú', 'ù', 'û', 'ü',
-        '!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '/', '?', '|', '\\', '`', '~', '^'
-      ];
+      'A', 'Á', 'À', 'Â', 'Ã', 'Ä',
+      'B',
+      'C', 'Ç',
+      'D',
+      'E' ,'É', 'È', 'Ê', 'Ë',
+      'F', 'G', 'H',
+      'I', 'Í', 'Ì', 'Î', 'Ï',
+      'J', 'L', 'M', 'N',
+      'O','Ó', 'Ò', 'Ô', 'Õ', 'Ö',
+      'P', 'Q', 'R', 'S', 'T',
+      'U', 'Ú', 'Ù', 'Û', 'Ü',
+      'V', 'X', 'Z',
+      'Y', 'W', 'K',
+      'á', 'à', 'â', 'ã', 'ä',
+      'ç',
+      'é', 'è', 'ê', 'ë',
+      'í', 'ì', 'î', 'ï',
+      'ó', 'ò', 'ô', 'õ', 'ö',
+      'ú', 'ù', 'û', 'ü',
+      '!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '/', '?', '|', '\\', '`', '~', '^'
+    ];
 
     let listOfSpecialTyped = [];
 
@@ -49,12 +62,9 @@ function detectedCharacters (array) {
 
 
     return listOfSpecialTyped;
-}
+  };
 
-
-function correctingCharacters (array) {
-   
-
+  correctingCharacters(array) {
     const specCharacters = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '/', '?', '|', '\\', '`', '~', '^'];
     let filterSpecialTyped = array.filter(char => !specCharacters.includes(char));
 
@@ -94,150 +104,97 @@ function correctingCharacters (array) {
     newTextLowerCase = newTextLowerCase.toLowerCase();
 
     return newTextLowerCase;
-      
-};
+  }
 
-
-cripty.addEventListener('click', (e)=>{
+  handleCripty (e) {
     e.preventDefault();
-    
-    const textValue = text.value;
 
+    const textValue = this.text.value;
     if(!textValue){
-        alert("Campo vazio. Digite seu texto!");
-        return
-    }
-
+      alert("Campo vazio. Digite seu texto!");
+      return
+    };
 
     const listLetter = textValue.split('');
-    const lettersToCode = ["a", "e", "i", "o", "u"]
+    const lettersToCode = ["a", "e", "i", "o", "u"];
 
-    const prohibitedCharacters = detectedCharacters(listLetter);
-
-
-    if(prohibitedCharacters.length !== 0){
-        modal.classList.remove("deactivate");
-        modal.classList.add("activate");
-        modalSpan.innerHTML = `Voce digitou um ou mais caracters proibidos ${prohibitedCharacters}. Deseja que o sistema corrija?`;
-
-        button_y.addEventListener("click", (e)=>{
-            e.preventDefault();
-
-            modal.classList.remove("activate");
-            modal.classList.add("deactivate");
-
-            let result = correctingCharacters(listLetter);
-            result = result.split('');
-    
-            result.forEach((letter, index) =>{
-            
-                if(index >= 0 && index < result.length){
-        
-                    lettersToCode.forEach(search =>{
-                        if(letter === search){
-                            if(search === "a"){result[index] = "ai"}
-                            if(search === "e"){result[index] = "enter"}
-                            if(search === "i"){result[index] = "imes"}
-                            if(search === "o"){result[index] = "ober"}
-                            if(search === "u"){result[index] = "ufat"}
-                        }
-                    })
-                }
-    
-                let newWordCripty = '';
-                result.forEach(letter => {
-                    newWordCripty += letter
-                })
-            
-                wordCripty.innerHTML = newWordCripty
-            
-                text.value = ""
-            } );
-
-            divWordCripty.classList.remove('deactivate');
-            contentNotFound.classList.add('deactivate');
-
-            return;
-        });
-
-        button_n.addEventListener("click", (e)=>{
-            e.preventDefault();
-
-            modal.classList.remove("activate");
-            modal.classList.add("deactivate");
-
-            text.value = '';
-
-            return;
-        })
+    const prohibitedCharacters = this.detectedCharacters(listLetter);
+    if(prohibitedCharacters.length !== 0) {
+      this.modal.classList.remove("deactivate");
+      this.modal.classList.add("activate");
+      this.modalSpan.innerHTML = `Voce digitou um ou mais caracters proibidos ${prohibitedCharacters}. Deseja que o sistema corrija?`;
+      
+      
     } else {
-        divWordCripty.classList.remove('deactivate');
-        contentNotFound.classList.add('deactivate');
-        listLetter.forEach((letter, index) =>{
-        
-            if(index >= 0 && index < listLetter.length){
-    
-                lettersToCode.forEach(search =>{
-                    if(letter === search){
-                        if(search === "a"){listLetter[index] = "ai"}
-                        if(search === "e"){listLetter[index] = "enter"}
-                        if(search === "i"){listLetter[index] = "imes"}
-                        if(search === "o"){listLetter[index] = "ober"}
-                        if(search === "u"){listLetter[index] = "ufat"}
-                    }
-                })
-            }
+      this.divWordCripty.classList.remove('deactivate');
+      this.contentNotFound.classList.add('deactivate');
 
-            let newWordCripty = '';
-            listLetter.forEach(letter => {
-                newWordCripty += letter
+      listLetter.forEach((letter, index) =>{
+        
+        if(index >= 0 && index < listLetter.length){
+
+            lettersToCode.forEach(search =>{
+                if(letter === search){
+                    if(search === "a"){listLetter[index] = "ai"}
+                    if(search === "e"){listLetter[index] = "enter"}
+                    if(search === "i"){listLetter[index] = "imes"}
+                    if(search === "o"){listLetter[index] = "ober"}
+                    if(search === "u"){listLetter[index] = "ufat"}
+                }
             })
-        
-            wordCripty.innerHTML = newWordCripty
-        
-            text.value = ""
-        } );
+        }
 
-        return;
+        let newWordCripty = '';
+        listLetter.forEach(letter => {
+            newWordCripty += letter
+        })
+    
+        this.wordCripty.innerHTML = newWordCripty
+    
+        this.text.value = ""
+      } );
+
+      return;
     };
-});
+  };
 
-copy.addEventListener("click", (e)=>{
+  handleCopy (e) {
     e.preventDefault();
 
-    const wordToCopy = wordCripty.innerHTML;
+    const wordToCopy = this.wordCripty.innerHTML;
     navigator.clipboard.writeText(wordToCopy);
-    alertCopy.classList.remove('deactivate');
-    alertCopy.classList.add('activate');
+    this.alertCopy.classList.remove('deactivate');
+    this.alertCopy.classList.add('activate');
 
     setTimeout(()=>{
-        alertCopy.classList.remove('activate');
-        alertCopy.classList.add('deactivate');
+        this.alertCopy.classList.remove('activate');
+        this.alertCopy.classList.add('deactivate');
     }, 2000)
 
-    wordCripty.innerHTML = ''
-    divWordCripty.classList.add('deactivate');
-    contentNotFound.classList.remove('deactivate');
-});
-clean.addEventListener("click", (e)=>{
-    e.preventDefault();
-    
-    wordCripty.innerHTML = ''
-    divWordCripty.classList.add('deactivate');
-    contentNotFound.classList.remove('deactivate');
-})
-descripty.addEventListener('click', (e)=>{
+    this.wordCripty.innerHTML = ''
+    this.divWordCripty.classList.add('deactivate');
+    this.contentNotFound.classList.remove('deactivate');
+  };
+
+  handleClean (e) {
+      e.preventDefault();
+      
+      this.wordCripty.innerHTML = ''
+      this.divWordCripty.classList.add('deactivate');
+      this.contentNotFound.classList.remove('deactivate');
+  }
+  handleDescripty (e) {
     e.preventDefault();
 
-    const textValue = text.value;
+    const textValue = this.text.value;
 
     if(!textValue){
-        alert("Campo vazio. Digite seu texto!");
-        return
-    }
+      alert("Campo vazio. Digite seu texto!");
+      return
+    };
 
-    divWordCripty.classList.remove('deactivate');
-    contentNotFound.classList.add('deactivate');
+    this.divWordCripty.classList.remove('deactivate');
+    this.contentNotFound.classList.add('deactivate');
 
     let newWords = textValue.replaceAll("ai", "a");
     newWords = newWords.replaceAll("*", "á");
@@ -246,8 +203,65 @@ descripty.addEventListener('click', (e)=>{
     newWords = newWords.replaceAll("ober", "o");
     newWords = newWords.replaceAll("ufat", "u");
 
-    wordCripty.innerHTML = newWords;
+    this.wordCripty.innerHTML = newWords;
 
-    text.value = ""
-})
+    this.text.value = ""
 
+  };
+
+  handleYes() {
+
+    this.modal.classList.remove("activate");
+    this.modal.classList.add("deactivate");
+
+    const textValue = this.text.value;
+    const listLetter = textValue.split('');
+
+    let result = this.correctingCharacters(listLetter);
+    result = result.split('');
+    const lettersToCode = ["a", "e", "i", "o", "u"];
+
+    result.forEach((letter, index)=>{
+      if(index >= 0 && index < result.length){
+    
+        lettersToCode.forEach(search =>{
+            if(letter === search){
+                if(search === "a"){result[index] = "ai"}
+                if(search === "e"){result[index] = "enter"}
+                if(search === "i"){result[index] = "imes"}
+                if(search === "o"){result[index] = "ober"}
+                if(search === "u"){result[index] = "ufat"}
+            };
+        });
+      };
+
+      let newWordCripty = '';
+      result.forEach(letter => {
+        newWordCripty += letter
+      })
+        
+      this.wordCripty.innerHTML = newWordCripty
+        
+      this.text.value = ""
+    });
+
+
+    this.divWordCripty.classList.remove('deactivate');
+    this.contentNotFound.classList.add('deactivate');
+
+    return;
+  };
+
+  handleNo(e) {
+    e.preventDefault();
+
+    this.modal.classList.remove("activate");
+    this.modal.classList.add("deactivate");
+
+    this.text.value = '';
+
+    return;
+  };
+};
+
+const textprocess = new TextProcess();
